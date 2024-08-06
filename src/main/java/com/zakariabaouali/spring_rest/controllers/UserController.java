8996c8add64cf0159459802007fa46a4b1b57cb1
zakaria <zakariabaouali@outlook.com>
2024-08-06 22:12:50 +0100

    create rest api for upload files

--- src/main/java/com/zakariabaouali/spring_rest/controllers/UserController.java
+++ src/main/java/com/zakariabaouali/spring_rest/controllers/UserController.java
@@ -1,14 +1,20 @@
 package com.zakariabaouali.spring_rest.controllers;
 
 import com.zakariabaouali.spring_rest.dto.UserDTO;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.swing.plaf.PanelUI;
+import java.io.File;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
 @RestController
 public class UserController {
+
+    private final String FOLDER_PATH = "C:\\Users\\PC\\Desktop\\Java\\spring-rest\\src\\main\\resources\\static\\";
 
     //get all users
     @GetMapping("/api/v1/users")
@@ -42,5 +48,29 @@ public class UserController {
         return "this query should filter by : " + filterBy;
     }
 
-    //api/v1/users/profile
+    //api/v1/users/avatar
+    @PostMapping("/api/v1/users/avatar")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createUserAvatar(@RequestParam("file") MultipartFile file){
+        String filePath = this.FOLDER_PATH  + file.getOriginalFilename();
+        try {
+            file.transferTo(new File(filePath));
+        }catch(Exception ex){
+            return "cannot save the file";
+        }
+        return "file uploaded name : " + file.getOriginalFilename();
+    }
+
+    @GetMapping("/api/v1/users/avatar/{avatar}")
+    @ResponseStatus(HttpStatus.OK)
+    public byte[] getUserAvatar(@PathVariable("avatar") String avatar){
+        String filePath = this.FOLDER_PATH + avatar;
+        byte[] image = null;
+        try {
+            image = Files.readAllBytes(new File(filePath).toPath());
+        }catch (Exception ex){
+            System.out.println("cannot get the file");
+        }
+        return image;
+    }
 }
