d5a6878e333c5df4c13ed1d90936bffe31058ec1
zakaria <zakariabaouali@outlook.com>
2024-08-06 23:58:48 +0100

    optimze the controller

--- src/main/java/com/zakariabaouali/spring_rest/controllers/UserController.java
+++ src/main/java/com/zakariabaouali/spring_rest/controllers/UserController.java
@@ -1,23 +1,23 @@
 package com.zakariabaouali.spring_rest.controllers;
 
 import com.zakariabaouali.spring_rest.dto.UserDTO;
-import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.swing.plaf.PanelUI;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/api/v1/users/")
 public class UserController {
 
     private final String FOLDER_PATH = "C:\\Users\\PC\\Desktop\\Java\\spring-rest\\src\\main\\resources\\static\\";
 
     //get all users
-    @GetMapping("/api/v1/users")
+    @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public ArrayList<String> getUsers() {
         var users = new ArrayList<String>();
@@ -27,7 +27,7 @@ public class UserController {
     }
 
     //create new user
-    @PostMapping("/api/v1/users")
+    @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public UserDTO createUser(@RequestBody UserDTO payload){
         return  payload;
@@ -35,21 +35,21 @@ public class UserController {
 
 
     //get user with id
-    @GetMapping("/api/v1/users/{id}")
+    @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public String getUserByID(@PathVariable("id") String id){
         return "user id : " + id;
     }
 
     ///api/v1/users/filter?filterBy=name
-    @GetMapping("/api/v1/users/filter")
+    @GetMapping("/filter")
     @ResponseStatus(HttpStatus.OK)
     public String filterUsers(@RequestParam() String filterBy){
         return "this query should filter by : " + filterBy;
     }
 
     //api/v1/users/avatar
-    @PostMapping("/api/v1/users/avatar")
+    @PostMapping("/avatar")
     @ResponseStatus(HttpStatus.CREATED)
     public String createUserAvatar(@RequestParam("file") MultipartFile file){
         String filePath = this.FOLDER_PATH  + file.getOriginalFilename();
@@ -61,7 +61,7 @@ public class UserController {
         return "file uploaded name : " + file.getOriginalFilename();
     }
 
-    @GetMapping("/api/v1/users/avatar/{avatar}")
+    @GetMapping("/avatar/{avatar}")
     @ResponseStatus(HttpStatus.OK)
     public byte[] getUserAvatar(@PathVariable("avatar") String avatar){
         String filePath = this.FOLDER_PATH + avatar;
